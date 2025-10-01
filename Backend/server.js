e8a7d62d711800bfd879997fac76546106f13809
Atharva Mandle <atharvamandle19@gmail.com>
2025-10-01 19:00:26 +0530

    fix1

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,10 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; // Use Render's assigned port or fallback
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
+});
 
 // Middleware
 app.use(cors());
